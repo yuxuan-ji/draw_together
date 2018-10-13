0c11a98b63a316146206b5da2bfcfbc961437e10
Y.J <yuxuan98@hotmail.com>
2018-10-12 23:47:35 -0400

    serving root through express

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var socket = require('socket.io');
+var path = require('path');
 
 var app = express();
-app.use(express.static('/build'));
+app.use(express.static(path.join(__dirname, '..\\..\\build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..\\..\\build', 'index.html'));
+});
 
 var PORT = process.env.PORT || 8000;
 var server = app.listen(PORT, function(){
